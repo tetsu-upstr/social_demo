2426ef0771ffee3145b8248a22814534a68aee4d
tetsu-upstr <bodomu@gmail.com>
2020-03-11 11:30:48 +0900

    first commit

---
+++ comment_frame.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <title>Document</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+  
+  <?php  
+  require 'config/config.php';
+  include("includes/classes/User.php");
+  include("includes/classes/Post.php"); 
+
+  if (isset($_SESSION['username'])) {
+    $userLoggedIn = $_SESSION['username'];
+    $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username = '$userLoggedIn'");
+    $user = mysqli_fetch_array($user_details_query);
+  }
+  else {
+    header("Location: register.php");
+  }
+
+?>
+<script>
+    function toggle() {
+      var element = document.getElementById("comment_section");
+      
+      if (element.style.display == "block")
+          element.style.display = "none";
+      else
+          element.style.display ="block";
+    }
+</script>
+
+<?php
+//get id of post
+if (isset($_GET['post_id'])) {
+  $post_id = $_GET['post_id'];
+}
+
+$user_query = mysqli_query($con, "SELECT added_by, user_to FROM posts id='$post_id'");
+$row = mysqli_fetch_array($user_query);
+
+$posted_to = $row['added_by'];
+
+?>
+
+</body>
+</html>
---
+++ profile.php
@@ -0,0 +1,13 @@
+<?php  
+include("includes/header.php");
+?>
+
+	<div class="main_column column">
+		
+
+	</div>
+      profile page
+	</div>
+
+</body>
+</html>
